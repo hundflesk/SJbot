4125cb3d93ac426aa33b8e312748420c553436f1
nissehult <37697552+nissehult@users.noreply.github.com>
2018-10-13 12:19:20 +0200

    Update: 2
    Added commands,fixed small bugs and made code better.

--- SJbot/Program.cs
+++ SJbot/Program.cs
@@ -24,45 +24,77 @@ namespace SJbot
 
     internal class SJCommands
     {
-        [Command("help")] //skriver ner alla kommandon som kan användas
-        public async Task Help(CommandContext ctx)
+        public static bool notifications = true; //notiser är aktiverade by default
+
+        [Command("notis")]
+        public async Task Notifications(CommandContext ctx, string setting)
         {
-            await ctx.RespondAsync($"{ctx.User.Mention}, här en lista med kommandon som du kan använda:");
+            string msg = null;
 
-            //skriv ut alla kommandon som kan användas
+            var setting0 = new string[] { "off", "deactivated"};
+            var setting1 = new string[] { "on", "activated" };
+            string setting2 = "state";
 
+            string note = "Setting has not been changed";
 
-            await ctx.RespondAsync($"Du kan använda ?{}");
-        }
-
-        [Command("command")] //skriver hur ett kommando fungerar
-        public async Task CommandHelp(CommandContext ctx, string command)
-        {
-            await ctx.RespondAsync($"{ctx.User.Mention}, ");
-        }
-
-        [Command("tåg")] //skriver vilka tåg som går inom de kommande 60 minuterna
-        public async Task NextTrain(CommandContext ctx)
-        {
-            var currentTime = ;
-            var timeInterval = currentTime.Add(new TimeSpan(1, 0, 0));
-
-            //kolla vilka tåg som går mellan 'currentTime' och 'timeInterval' (60 min)
-            foreach (var train in Program.trainList)
+            if (setting == setting0[0] && notifications == true)
             {
-                if (train.departure > currentTime && train)
-                {
-
-                }
+                notifications = false;
+                msg = $"Notifications have been {setting0[1]}.";
+            }
+            else if (setting == setting1[0] && notifications == false)
+            {
+                notifications = true;
+                msg = $"Notifications have been {setting1[1]}.";
             }
 
-            await ctx.RespondAsync($"");
+            else if (setting == setting0[0] && notifications == false)
+                msg = $"Notifications are already {setting0[1]}. {note}.";
+
+            else if (setting == setting1[0] && notifications == true)
+                msg = $"Notifications are already {setting1[1]}. {note}.";
+
+            else if (setting == setting2)
+            {
+                if (notifications == false)
+                    msg = $"Notifications are currently {setting0[1]}.";
+                else if (notifications == true)
+                    msg = $"Notifications are currently {setting1[1]}.";
+            }
+            await ctx.RespondAsync(msg);
         }
 
-        [Command("notis")] //
-        public async Task Notification(CommandContext ctx)
+        [Command("tåg")]
+        public async Task Train(CommandContext ctx)
         {
+            string msg = null;
 
+            var currentDay = DateTime.Now;
+            if (currentDay.DayOfWeek == DayOfWeek.Saturday || currentDay.DayOfWeek == DayOfWeek.Sunday)
+                msg = $"{ctx.User.Mention}, the trains will not run on weekends.";
+            else
+            {
+                var currentTime = new TimeSpan(currentDay.Hour, currentDay.Minute, currentDay.Second);
+                var timeInterval = currentTime.Add(new TimeSpan(1, 0, 0));
+
+                msg = $"{ctx.User.Mention}, in this coming hour will the following run:";
+                int trainQuantity = 0;
+
+                //kolla vilka tåg som går mellan 'currentTime' och 'timeInterval' (60 min)
+                foreach (var train in Program.trainList)
+                {
+                    if (train.departure > currentTime && train.departure < timeInterval)
+                    {
+                        msg += $" Train {train.num} at {train.departure} from track {train.track}.";
+                        trainQuantity++;
+                    }
+                }
+                if (trainQuantity == 0)
+                    msg = $"{ctx.User.Mention}, there is no trains which runs this coming hour.";
+                else if (trainQuantity == 1)
+                    msg += " It seems like it is only hourtrains which runs right now.";
+            }
+            await ctx.RespondAsync(msg);
         }
     }
 
@@ -71,18 +103,19 @@ namespace SJbot
         private static DiscordClient discord;
         private static CommandsNextModule commands;
 
-        public static List<Tåg> trainList = AddTrains();
+        public static List<Tåg> trainList;
 
         private static void Main(string[] args)
         {
+            trainList = AddTrains();
             MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
         }
 
         private static List<Tåg> AddTrains()
         {
-            var trains = new List<Tåg>();
+            var trains = new List<Tåg>(); //lista med alla tåg som avgår på en dag
 
-            var timeForFirstTrain = new TimeSpan(9, 14, 0); //första tåget går 08:14
+            var timeForFirstTrain = new TimeSpan(9, 14, 0); //första tåget går 09:14
             var timeForLastTrain = new TimeSpan(19, 14, 0); //sista tåget går 19:14
             var timeToAdd = new TimeSpan(0, 30, 0); //tågen går vanligtvis varje halvtimme
 
@@ -94,15 +127,15 @@ namespace SJbot
                 {794, 13 }, {756, 14 }, {796, 13 }, {760, 14 }
             }.ToList();
 
-            int lastHourTrainIndex = 6; //är indexet för det sista tåget i 'trainsInfo' som går varje timme
+            int lastHourTrainIndex = 5; //är indexet för det sista tåget i 'trainsInfo' som går varje timme
             int index = 0; //är indexet som går igenom alla tåg i 'trainsInfo'
 
             for (var time = timeForFirstTrain; time <= timeForLastTrain; time = time.Add(timeToAdd))
             {
                 trains.Add(new Tåg(trainsInfo[index].Key, trainsInfo[index].Value, time));
 
-                //de första sex tågen går varje timme, därför läggs ytterligare 30 min till
-                //från och med det 7:e tåget går resten av tågen var 30:e minut istället
+                //de första 5 tågen går varje timme, därför läggs ytterligare 30 min till
+                //från och med det 6:e tåget går resten av tågen var 30:e minut istället
                 if (index < lastHourTrainIndex)
                     time = time.Add(timeToAdd);
 
@@ -111,9 +144,14 @@ namespace SJbot
 
             foreach (var t in trains)
             {
-                Console.WriteLine(string.Format("Tåg {0} avgår klockan {01}:{2} från spår {3}",
+                Console.WriteLine(string.Format("Tåg {0} avgår klockan {1:00}:{2:00} från spår {3}",
                     t.num, t.departure.Hours, t.departure.Minutes, t.track));
             }
+
+            if (index == trainsInfo.Count)
+                Console.WriteLine("\nSuccess: alla tågen har lagts till i listan.");
+            else
+                Console.WriteLine("\nError: listan saknar tåg.");
 
             return trains;
         }
